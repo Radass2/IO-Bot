a49834a997a5aa47bfbdc20295778d5db788fd72
Radass2 <32589207+Radass2@users.noreply.github.com>
2017-10-07 00:15:22 -0700

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,15 @@
+const discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready,() => (
+    console.log('I am ready!');
+    });
+    
+    client.on('message',message => ( 
+    if(message.content === 'ping') {
+      message.reply('68');
+         }
+});
+
+// THIS MUST BE THIS WAY
+  client.login(process.env.BOT_TOKEN);
